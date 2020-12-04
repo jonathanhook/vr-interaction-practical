b58ea25157cbeb7383c5a1401a2c89df570e94c0
Jonathan Hook <jonathan.hook+github@gmail.com>
2020-12-04 11:17:10 +0000

    Any changes

--- Assets/Practical Assets/EmulatorInput.cs
+++ Assets/Practical Assets/EmulatorInput.cs
@@ -10,7 +10,17 @@ public class EmulatorInput : MonoBehaviour
     }
 
     void Update()
-    { 
+    {
+        /* When completing the optional HTC Vive practical exercises in your 
+         * indivdiual access slot, you should be able to adapt your code to work 
+         * based on the buttons on the Vive controller by replacing the calls
+         * to Input.GetMouse... with calls to the SteamVR API. For example, you
+         * would use code allow the following lines to get check if the trigger
+         * under the left controller is pressed / released:
+         * 
+         * SteamVR_Input._default.inActions.GrabPinch.GetStateDown(SteamVR_Input_Sources.LeftHand)
+         * SteamVR_Input._default.inActions.GrabPinch.GetStateUp(SteamVR_Input_Sources.LeftHand)
+         */
         if (Input.GetMouseButtonDown(0))
         {
             OnVRControllerButtonPressed();
